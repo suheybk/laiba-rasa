2cf7ea4ed26a9146fd1d45b8d6436f895c85909e
suheybk <suheybk@gmail.com>
2026-01-10 09:30:34 +0300

    fix: suppress hydration warning for browser extensions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="tr" dir="ltr">
+    <html lang="tr" dir="ltr" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased animated-gradient min-h-screen`}
       >
